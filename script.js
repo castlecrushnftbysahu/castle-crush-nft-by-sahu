33e13fc2bdd2ebebed84b3052f84e591b7ebd766
castlecrushnftbysahu <ujjawalgupta721@gmail.com>
2026-01-22 21:53:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 let selectedItem = "";
-let selectedPrice = "";
+let selectedPrice = 0;
 
 function buyItem(item, price){
   selectedItem = item;
   selectedPrice = price;
-  document.getElementById("qrImage").src = "images/qr.png";
+
+  const upiLink =
+    "upi://pay?pa=9142053259@upi&pn=CastleCrush&am=" +
+    price +
+    "&cu=INR&tn=" +
+    encodeURIComponent(item);
+
+  const qrURL =
+    "https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" +
+    encodeURIComponent(upiLink);
+
+  document.getElementById("qrImage").src = qrURL;
   document.getElementById("qrModal").style.display = "block";
 }
 
 function sendProof(){
   const msg =
-    "payment proof\n\n" +
-    "item: " + selectedItem +
-    "\npayment done via qr.\n" +
-    "screenshot attached.";
+    "payment proof\nitem: " +
+    selectedItem +
+    "\namount: ₹" +
+    selectedPrice;
 
   window.open(
     "https://wa.me/919142053259?text=" +
@@ -23,7 +34,7 @@ function sendProof(){
 }
 
 function closeQR(){
-  document.getElementById("qrModal").style.display = "none";
+  document.getElementById("qrModal").style.display="none";
 }
 
 function downloadQR(){
